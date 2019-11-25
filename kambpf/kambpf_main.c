0827fedb5256d688e0cf8a3431953a3eed8de678
Dusan Zivanovic <zdule1998@gmail.com>
2019-11-25 00:47:48 +0000

    Implemented update ioctl. Largely untested.

--- kambpf/kambpf_main.c
+++ kambpf/kambpf_main.c
@@ -26,27 +26,52 @@ module_param(max_num_pages, uint, S_IRUGO);
 
 // ======================= kambpf_list_dev datatypes ==============================
 
+/*
+   CAUTION!!
+   The size of this struct must be a power of two (32bytes currently)
+   in order for an array of these to always align with page boundaries.
+*/
+
 struct probe_table_entry {
     unsigned long instruciton_address;
-    unsigned long call_destination;
-    void *ebpf_program;
-    void *data;
-    struct list_head empty_entries;
+    union {
+        struct {
+            unsigned long call_destination;
+            void *ebpf_program;
+            void *data;
+        };
+        struct list_head empty_entries;
+    };
 };
+_Static_assert(sizeof(struct probe_table_entry) == 32,
+        "The probe_table_entry struct must have a power of two size");
 
 /* The probe_table header is contained at the start of the first page.
    After it the table entries are laid out. */
+
 struct probe_table_header {
-    size_t num_probes;
+    unsigned long num_entries;
+    unsigned long start_offset;
+    unsigned long max_num_entries;
+    unsigned long _pad;
 };
+
+_Static_assert(sizeof(struct probe_table_header) == sizeof(struct probe_table_entry),
+        "The size of probe_table handler must be the same as the size of probe_table_entry \
+        because these have to align in a page");
 
 struct probe_table {
     struct probe_table_header *header;
+    int num_active_probes;
+
     size_t num_pages;
-    u8 **pages;
+    struct probe_table_entry **pages;
     struct list_head emptry_entries;
 };
 
+const u32 entries_per_page = PAGE_SIZE / sizeof(struct probe_table_entry);
+const u32 entries_per_header = sizeof(struct probe_table_header) / 
+                               sizeof(struct probe_table_entry);
 
 struct kambpf_list_dev {
     struct cdev cdev; 
@@ -59,13 +84,13 @@ struct kambpf_list_dev list_dev;
 
 int probe_table_init(struct probe_table *table) {
     table->num_pages = 1;
-    table->pages = (u8**) kzalloc(sizeof(u8*)*max_num_pages, GFP_KERNEL);
+    table->pages = (struct probe_table_entry **) kzalloc(sizeof(u8*)*max_num_pages, GFP_KERNEL);
     if (!table->pages) {
         printk(KERN_WARNING MODULE_NAME ": unable to allocate memory for the probe table.\n");
         return -ENOMEM;
     }
 
-    table->pages[0] = (u8*) __get_free_page(GFP_KERNEL);
+    table->pages[0] = (struct probe_table_entry *) get_zeroed_page(GFP_KERNEL);
     if (!table->pages[0]) {
         printk(KERN_WARNING MODULE_NAME ": unable to allocate a memory page for the probe table.\n");
         kfree(table->pages);
@@ -73,7 +98,11 @@ int probe_table_init(struct probe_table *table) {
     }
 
     table->header = (struct probe_table_header*) table->pages[0];
-    table->header->num_probes = 0;
+    table->header->num_entries = 0;
+    table->header->start_offset = sizeof(struct probe_table_header);
+    table->header->max_num_entries = max_num_pages*entries_per_page - entries_per_header;
+
+    table->num_active_probes = 0;
 
     INIT_LIST_HEAD(&table->emptry_entries);
     return 0;
@@ -86,6 +115,90 @@ void probe_table_cleanup(struct probe_table *table) {
     kfree(table->pages);
 }
 
+void probe_table_erase(struct probe_table *table, u32 index) {
+    struct probe_table_entry *e;
+    kamprobe *kp;
+
+    // acount for the header, which takes space from the first page
+    index += entries_per_header;
+
+    e = table->pages[index/entries_per_page] + (index % entries_per_page);
+    
+    // Address is zero iff the probe is inactive, no work to be done here.
+    // Aditionally must not reinsert entry into the empty_entries list
+    if (e->instruciton_address == 0) return;
+
+    kp = (kamprobe *) e->data;
+    memset(e, 0, sizeof(struct probe_table_entry));
+    kamprobe_unregister(kp);
+    list_add(&e->empty_entries, &table->emptry_entries);
+    table->num_active_probes--;
+}
+
+bool probe_table_can_accept(struct probe_table *table, u32 additional) {
+    return table->header->max_num_entries >= table->num_active_probes + additional;
+}
+
+int probe_table_add_page(struct probe_table *table) {
+    size_t next_page = table->num_pages;
+    size_t i;
+    if (next_page == max_num_pages) {
+        return -EINVAL;
+    }
+    table->pages[next_page] = 
+        (struct probe_table_entry *) get_zeroed_page(GFP_KERNEL);
+    if (!table->pages[next_page]) {
+        return -ENOMEM;
+    } 
+    for(i = 0; i < entries_per_page; i++) {
+        list_add(&table->pages[next_page][i].empty_entries, &table->emptry_entries);
+    }
+    table->num_pages++;
+    return 0;
+}
+
+u32 probe_table_addr_to_index(struct probe_table *table, 
+                              struct probe_table_entry *ptr) {
+    size_t i;
+    u32 off;
+    for(i = 0; i < table->num_pages; i++)
+        if (ptr >= table->pages[i] && ptr < table->pages[i] + PAGE_SIZE) {
+            off = ptr-table->pages[i];
+            return i*entries_per_page + off - entries_per_header;
+        }
+    return -EINVAL;
+}
+
+struct probe_table_entry *pop_empty_entry(struct probe_table * table) {
+    int err;
+    struct probe_table_entry *e;
+    if (list_empty(&table->emptry_entries)) {
+        err = probe_table_add_page(table);
+        if (err)
+            return ERR_PTR(err);
+    }
+    e = list_first_entry(&table->emptry_entries, struct probe_table_entry, empty_entries);
+    list_del(&e->empty_entries); 
+    return e; 
+}
+
+int probe_table_insert(struct probe_table *table, unsigned long address,
+                        u32 bpf_program_fd, u32 *index) {
+    struct probe_table_entry *e = pop_empty_entry(table);
+    kamprobe *kp = 0;
+    
+    if (IS_ERR(e)) { 
+        *index = PTR_ERR(e);
+        return PTR_ERR(e);
+    }
+    /* TODO INIT KAMPROBE */
+    e->data = kp;
+    e->ebpf_program = 0;
+    e->call_destination = 0;
+    e->instruciton_address = address;
+    *index = probe_table_addr_to_index(table, e);
+    table->num_active_probes++;
+}
 
 // ===================== kambpf_list file_operations =========================
 
@@ -178,11 +291,18 @@ void kambpf_list_dev_cleanup(struct kambpf_list_dev *dev) {
 
 // ======================== kambpf_update_dev datatypes =======================
 
+/*
+  CAUTION!!
+  The size of this struct needs to be a power of two, so that an array of these
+  can fill a memory page.
+  Also note that this code is not portable. It assumes x86-64 and that addresses
+  are 64bit.
+*/
 struct kambpf_update_entry {
-    unsigned long instruction_address;
-    __u32 bpf_program_fd;   // __u32 to be accessible to the userland
+    __u64 instruction_address;
+    __u32 bpf_program_fd;
     // Set by the module
-    size_t table_pos; 
+    __u32 table_pos; 
 };
 
 struct kambpf_update_buffer {
@@ -233,8 +353,47 @@ void kambpf_update_buffer_free(struct kambpf_update_buffer *update_buffer) {
     kfree(update_buffer);
 }
 
-int process_updates(unsigned long updates_count) {
 
+long update_buffer_prefix(struct kambpf_update_buffer *update_buffer) {
+    unsigned long s = 0;
+    size_t i;
+    for(i = 0; i < max_num_pages && update_buffer->pages[i]; i++)
+        s += PAGE_SIZE;
+    return s;
+}
+
+void process_update_entry(struct kambpf_update_entry *entry) {
+    // if address == 0 then we remove the item in table_pos
+    if (entry->instruction_address == 0) {
+        probe_table_erase(&list_dev.table, entry->table_pos);
+    } else {
+        probe_table_insert(&list_dev.table, entry->instruction_address, 
+                            entry->bpf_program_fd, &entry->table_pos);
+    }
+}
+
+long process_updates(struct kambpf_update_buffer * update_buffer,
+                     unsigned long updates_count) {
+    unsigned long off;
+    unsigned long i;
+    unsigned long pgnum;
+    unsigned long prefix_bytes = update_buffer_prefix(update_buffer); 
+    size_t entry_width = sizeof(struct kambpf_update_entry);
+
+    printk(KERN_INFO MODULE_NAME ": hello from ioctl %lu!\n", updates_count);
+    if (prefix_bytes / entry_width < updates_count)
+        return -EINVAL;
+    
+    pgnum = 0; 
+    off = 0;
+    for (i = 0; i < updates_count; i++) {
+        if (off >= PAGE_SIZE) {
+            off = 0;
+            pgnum++;
+        } 
+        process_update_entry(update_buffer->pages[pgnum] + off);
+        off += entry_width;
+    }
     return 0;
 }
 
